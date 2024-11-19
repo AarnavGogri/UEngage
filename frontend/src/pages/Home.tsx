935336c2aa3879071499c05642ca266719d1d356
AarnavGogri <aarnav.gogri@gmail.com>
2024-11-19 00:06:38 -0800

    debugged code

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -15,15 +15,3 @@ const Home: React.FC = () => (
 );
 
 export default Home;
-
-
-// import React from 'react';
-
-// const Home: React.FC = () => (
-//   <div>
-//     <h1>Welcome to the Community Engagement App</h1>
-//     <p>Connect with clubs and stay updated on events.</p>
-//   </div>
-// );
-
-// export default Home;
