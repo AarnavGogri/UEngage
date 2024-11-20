13974583138791415f0e3f9cfc751643bebd94a9
Aryaman Tandon <aryaman.tandon@gmail.com>
2024-11-19 18:25:08 -0800

    fixed reload issue

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -42,25 +42,3 @@ const NavBar: React.FC = () => {
 };
 
 export default NavBar;
-// import { Link } from 'react-router-dom';
-// import { useAuth } from '../context/AuthContext';
-
-// const NavBar: React.FC = () => {
-//   const { currentUser } = useAuth();
-
-//   return (
-//     <nav>
-//       <Link to="/">Home</Link> | <Link to="/clubs">Clubs</Link> | <Link to="/events">Events</Link>
-//       {currentUser ? (
-//         <>
-//           {' '}
-//           | <Link to="/profile">Profile</Link> | <Link to="/create-club">Create Club</Link> | <button>Logout</button>
-//         </>
-//       ) : (
-//         <> | <Link to="/login">Login</Link></>
-//       )}
-//     </nav>
-//   );
-// };
-
-// export default NavBar;
