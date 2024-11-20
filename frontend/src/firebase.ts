f153403d603942a280f16ec6332c8e5aed2d8b82
trivedi2516 <rahultrivedi2205@gmail.com>
2024-11-19 18:30:02 -0800

    hi

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -5,7 +5,15 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration (replace with your actual config)
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBFD-9ya7u780CV2qNWpbm6Zh9ow1c1h8s", //process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: "uengage-278e4.firebaseapp.com",
+    projectId: "uengage-278e4",
+    storageBucket: "uengage-278e4.appspot.com",
+    messagingSenderId: "382763489448",
+    appId: "1:382763489448:web:db689a25c9ff4211db4a9d",
+    measurementId: "G-SHZHRKXDJY"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
